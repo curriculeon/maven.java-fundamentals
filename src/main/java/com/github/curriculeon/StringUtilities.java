996704d9633968c4d575bb24d00abb5dc51e6c9e
Brent <BrentC0328@gmail.com>
2022-07-12 18:25:12 -0400

    Strings first 3 finished

--- src/main/java/com/github/curriculeon/StringUtilities.java
+++ src/main/java/com/github/curriculeon/StringUtilities.java
@@ -7,8 +7,9 @@ public class StringUtilities {
     /**
      * @return `Hello World` as a string
      */
+
     public static String getHelloWorld() {
-        return null;
+        return "Hello World";
     }
 
     /**
@@ -17,7 +18,7 @@ public class StringUtilities {
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
     public static String concatenation(String firstSegment, String secondSegment){
-        return null;
+        return (firstSegment + secondSegment);
     }
 
     /**
@@ -26,7 +27,7 @@ public class StringUtilities {
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
     public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     /**
